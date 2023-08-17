c8524894b099dc70c9d4d7d5f91b22ae80b37b12
Aleksey Rudyk <95295778+Ackermannn7@users.noreply.github.com>
2023-08-17 19:39:14 +0300

    onTopScrollButton

--- src/components/onTopScrollButton.jsx
+++ src/components/onTopScrollButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const OnTopScrollButton = () => {
+export const OnTopScrollButton = () => {
   const [isVisible, setIsVisible] = React.useState(false);
   const toggleVisibility = () => {
     if (window.scrollY > 500) {
@@ -48,5 +48,3 @@ const OnTopScrollButton = () => {
     </div>
   );
 };
-
-export default OnTopScrollButton;
